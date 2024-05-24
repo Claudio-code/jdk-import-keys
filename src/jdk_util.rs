8eab7a58765f8c57a48d3e256ff4ee1c0f8e7533
“claudio-code” <suporte2424@outlook.com>
2024-05-24 12:44:26 -0300

    changed list jdks to use lambda

--- src/jdk_util.rs
+++ src/jdk_util.rs
@@ -1,4 +1,8 @@
+use crate::collection_jdk::{CollectionJdk, CollectionJdkData};
+use crate::key_object::KeyData;
+use adw::subclass::prelude::*;
 use clap::Parser;
+use gtk::glib;
 use std::fmt::Debug;
 use std::fs::{DirEntry, ReadDir};
 use std::io::{Error, ErrorKind, Read};
@@ -6,10 +10,6 @@ use std::process::{Child, ExitStatus};
 use std::str::FromStr;
 use std::{fs, path::PathBuf, process::Command};
 use strum_macros::EnumString;
-use adw::subclass::prelude::*;
-use gtk::glib;
-use crate::collection_jdk::{CollectionJdk, CollectionJdkData};
-use crate::key_object::KeyData;
 
 const ASDF_JAVA_PATH: &str = "/.asdf/installs/java";
 const JETBRAINS_JAVA_PATH: &str = "/.jdks";
@@ -19,18 +19,28 @@ const KEYTOOL_PATH: &str =
 
 #[derive(Debug)]
 struct Dir {
-    path: DirEntry,
     path_name: String,
     is_dir: bool,
 }
 
 impl Dir {
+    pub fn new(dir: DirEntry) -> Self {
+        Self {
+            is_dir: dir.path().is_dir(),
+            path_name: dir.path().to_str().unwrap().to_string(),
+        }
+    }
+
     pub fn jdk_name(&self) -> &str {
         self.path_name.split("/").last().unwrap()
     }
 
+    pub fn path_name(&self) -> String {
+        self.path_name.clone()
+    }
+
     pub fn is_valid_dir(&self) -> bool {
-        self.path_name.contains("current") && self.is_dir
+        !self.path_name.contains("current") && self.is_dir
     }
 }
 
@@ -42,72 +52,69 @@ pub fn list_all_sdks() -> Vec<CollectionJdk> {
     all_sdks
 }
 
-
 fn load_jdks_asdf() -> Vec<CollectionJdk> {
+    if dirs::home_dir().is_none() {
+        return Vec::new();
+    }
     let binding = dirs::home_dir().unwrap();
     let mut home_dir = binding.display().to_string();
     home_dir.push_str(ASDF_JAVA_PATH.into());
-
-    let paths = fs::read_dir(home_dir);
-    if let Ok(paths) = paths {
+    if let Ok(paths) = fs::read_dir(home_dir) {
         return list_jdks(paths, "Asdf");
     }
     return Vec::new();
 }
 
 fn load_jdks_jetbrains() -> Vec<CollectionJdk> {
+    if dirs::home_dir().is_none() {
+        return Vec::new();
+    }
     let binding = dirs::home_dir().unwrap();
     let mut home_dir = binding.display().to_string();
     home_dir.push_str(JETBRAINS_JAVA_PATH.into());
-
-    let paths = fs::read_dir(home_dir);
-    if let Ok(paths) = paths {
+    if let Ok(paths) = fs::read_dir(home_dir) {
         return list_jdks(paths, "Jetbrains");
     }
     return Vec::new();
 }
 
 fn load_jdks_sdkman() -> Vec<CollectionJdk> {
+    if dirs::home_dir().is_none() {
+        return Vec::new();
+    }
     let binding = dirs::home_dir().unwrap();
     let mut home_dir = binding.display().to_string();
     home_dir.push_str(SDKMAN_JAVA_PATH.into());
-
-    let paths = fs::read_dir(home_dir);
-    if let Ok(paths) = paths {
+    if let Ok(paths) = fs::read_dir(home_dir) {
         return list_jdks(paths, "Sdkman");
     }
     return Vec::new();
 }
 
 fn list_jdks(paths: ReadDir, package_manager: &str) -> Vec<CollectionJdk> {
-    let mut jdk_collections: Vec<CollectionJdk> = Vec::new();
-    for path_result in paths {
-        let path = path_result.unwrap();
-        let path_name = path.path().to_str().unwrap().to_string();
-        let is_dir = path.path().is_dir();
-
-        if !path_name.contains("current") && is_dir {
-            let jdk_name = path_name.split("/").last().unwrap();
-            let jdk_path = path_name.clone();
-            let keys: Vec<KeyData> = list_certs_jdk(path_name.clone())
+    paths
+        .into_iter()
+        .map(Result::unwrap)
+        .map(Dir::new)
+        .filter(Dir::is_valid_dir)
+        .map(move |dir| {
+            let keys: Vec<KeyData> = list_certs_jdk(dir.path_name())
                 .into_iter()
-                .map(|key| KeyData { jdk_path: jdk_path.clone(), content: key })
+                .map(|key| KeyData {
+                    jdk_path: dir.path_name(),
+                    content: key,
+                })
                 .collect();
-            let title = format!("Java-{}", jdk_name);
-            let collection_jdk_data = CollectionJdkData { 
-                title: format!("Java-{}", jdk_name),
+            let collection_jdk_data = CollectionJdkData {
+                title: format!("Java-{}", dir.jdk_name()),
                 package_manager: package_manager.to_string(),
-                path: path_name,
-                keys: keys.clone()
+                path: dir.path_name,
+                keys: keys.clone(),
             };
-            let jdk_collection = CollectionJdk::new(collection_jdk_data);
-            jdk_collections.push(jdk_collection);
-        }
-    }
-    jdk_collections
+            return CollectionJdk::new(collection_jdk_data);
+        })
+        .collect()
 }
-
-
 
 fn list_certs_jdk(jdk_home_dir: String) -> Vec<String> {
     let command = format!("{}/bin/keytool -list -v -keystore {}/lib/security/cacerts -storepass changeit | grep Alias", jdk_home_dir, jdk_home_dir);
@@ -131,9 +138,7 @@ fn spawn_child(command: String) -> std::io::Result<Child> {
 fn keytool_capture(command: String) -> std::io::Result<String> {
     let mut child = spawn_child(command)?;
     match child.wait()?.code() {
-        Some(code) if code == 0 => {} // success
-        Some(code) if code == 1 => {} // success -> Ok(keys not found)
-        Some(code) if code == 3 => {} // success -> Ok(keys not found or error)
+        Some(code) if code == 0 || code == 1 || code == 3 => {}
         Some(code) if code == 4 => {
             return Err(Error::new(
                 ErrorKind::PermissionDenied,
